17396ee93aa10dd551124a777bb17946ff45f655
sysong <sysong@sysnew.com>
2022-11-29 23:23:04 +0800

    initial

---
+++ src/main/java/tech/getarrays/employeemanager/model/Employee.java
@@ -0,0 +1,20 @@
+package tech.getarrays.employeemanager.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    private String employeeCode;
+
+    
+}
